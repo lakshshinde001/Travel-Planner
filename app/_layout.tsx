b8a978c985a6de961dab7784f255e84969beaa1a
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-12-28 17:40:34 +0530

    Added Custom Font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 
 import { Stack } from 'expo-router';
-
+import {useFonts} from 'expo-font';
 
 export default function RootLayout() {
+
+  useFonts({
+    'outfit' : require('./../assets/fonts/Outfit-Black.ttf'),
+    'outfit-medium' : require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold' : require('./../assets/fonts/Outfit-Bold.ttf')
+  })
   return (
       <Stack>
         <Stack.Screen name="Home" options={{ headerShown: true }} />
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,12 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Hello World 2</Text>
+   <View style={{ flex: 1 }}>
+
+    <Text style={{
+        fontSize:30,
+        fontFamily: 'outfit-medium'
+    }} >Hello World 2</Text>
    </View>
   );
 }
