26c21d8124455b0e1c49f99e00cc1cf630683593
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-12-28 17:26:27 +0530

    Basic setup done

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Hello World 2</Text>
+   </View>
+  );
+}
+
+
